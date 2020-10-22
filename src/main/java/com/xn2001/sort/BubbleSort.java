b9a3e9869fc4f15d378fb9c4bfd99ba2ce5a3dcb
lexinhu <jialna@qq.com>
2020-10-22 11:06:16 +0800

    冒泡排序

---
+++ src/main/java/com/xn2001/sort/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.xn2001.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 1, 4, 3, 10, 556, 14};
+        System.out.print("整理前： ");
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+        System.out.print("整理后： ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean flag = false; // 标识变量，表示是否进行过交换
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+            if (!flag) { // 在一趟排序中，一次交换都没有发生过
+                System.out.println("一共仅执行了" + i + "次");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/xn2001/sort/SelectSort.java
@@ -0,0 +1,9 @@
+package com.xn2001.sort;
+
+public class SelectSort {
+    public static void main(String[] args) {
+
+    }
+
+
+}
